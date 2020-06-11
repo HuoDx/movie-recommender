95f22d351d13ad9de7882d428db719fa1d418181
Miranda Bian <bianyb@163.com>
2020-06-12 00:20:06 +0800

    Miranda0611

--- business_logic/sort_movies.py
+++ business_logic/sort_movies.py
@@ -4,8 +4,16 @@ from utils.ui import display_options
 # Miranda.
 def  sort_movies():
     # Start coding here
-    
-    print(movies.get_all_movies())
+    unsorted = movies.get_all_movies()
+    switched = True
+    while switched:
+        switched = False
+        for i in range(len(unsorted)-1):
+            user = input('Which one do you enjoy more? Enter 1 for '+str(unsorted[i])+' Enter 2 for '+str(unsorted[i+1]))
+            if user == '2':
+                switched = True
+                unsorted[i],unsorted[i+1]=unsorted[i+1],unsorted[i]
+    print(unsorted)
 
 sort_movies()
 #sara trying
