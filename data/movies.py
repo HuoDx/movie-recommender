3748187c18217c10f245efdf65f7429e73be4bd8
Maggie158 <53351089+Maggie158@users.noreply.github.com>
2020-06-29 15:34:29 +0800

    Update movies.py

--- data/movies.py
+++ data/movies.py
@@ -9,6 +9,12 @@ def save_to_file():
     global stored_movies
     # Save data to file
     # Start coding here
+    f = open('stored_movies.txt', 'w', encoding=‘utf-8’)
+    for movie in stored_movie:
+        f.write(movie)
+        f.write("\n")
+
+
 
 def add_movie(movie):
     global stored_movies
